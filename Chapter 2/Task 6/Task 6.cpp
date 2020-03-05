2af6541d6e99630077c3fc839d1cbde8a163940a
Chance20022 <chance20022@gmail.com>
2020-03-05 23:45:14 +0300

    Co C++

---
+++ Chapter 2/Task 6/Task 6.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float dollar = 0;
+
+    cout << "Enter the amount in dollar: ";
+    cin >> dollar;
+    cout << endl;
+    cout << "The amount in the round - " << dollar * 1.487 << endl;
+    cout << "The anount in the franc - " << dollar * 0.172 << endl;
+    cout << "The anoumt in the german mark - " << dollar * 0.584 << endl;
+    cout << "The anoumt in the yen - " << dollar * 0.00955;
+}
