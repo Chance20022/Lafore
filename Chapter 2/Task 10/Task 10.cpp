9488b1d26900d2d7f9bca04bbbb33ccca0bccc0a
Chance20022 <chance20022@gmail.com>
2020-03-06 02:14:52 +0300

    Не доделал

---
+++ Chapter 2/Task 10/Task 10.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float pounds = 0; //7
+    float shillings = 0; // 17
+    float pence = 0; // 9
+    int Modpunds = 0;
+
+    cout << "Enter the number of pounds: ";
+    cin >> pounds;
+    cout << "Enter the number of shillings: ";
+    cin >> shillings;
+    cout << "Enter the number of pence: ";
+    cin >> pence;
+    cout << endl;
+
+    Modpunds = ((shillings * 12) + pence) / 2.4;
+
+    cout << "Convert to modern pounds: " << pounds << Modpunds;
+
+}
