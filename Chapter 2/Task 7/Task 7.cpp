503a4faebdb539b92990bcf9049521a47ee26b7c
Chance20022 <chance20022@gmail.com>
2020-03-06 00:23:33 +0300

    Co C++

---
+++ Chapter 2/Task 7/Task 7.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float celsius = 0;
+
+    cout << "Enter a celsius - ";
+    cin >> celsius;
+    cout << endl;
+
+    celsius = (celsius * 9 / 5) + 32;
+
+    cout << "Celsius in Farangate - " << celsius;
+}
---
+++ Chapter 2/Task 8/Task 8.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    cout << "Moscow" << setfill('.') << setw(12) << "8425785";
+}
