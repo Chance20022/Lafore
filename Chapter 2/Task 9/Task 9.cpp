02255d02e6e1e07bcd7f2888b6ab33b53f06908d
Chance20022 <chance20022@gmail.com>
2020-03-06 01:16:26 +0300

    Co C++

---
+++ Chapter 2/Task 9/Task 9.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+float operation1(float x1, char op, float x2) {
+
+    float x = 0;
+
+    if (op == '/') {
+        x = x1 / x2;
+        return x;
+    }
+    else {
+        cout << "Bullshit with operation";
+        return false;
+    }
+
+}
+
+int main()
+{
+    float x1 = 0;
+    char op = ' ';
+    float x2 = 0;
+    cout << "Inter the first fraction:";
+    cin >> x1 >> op >> x2;
+
+    float sum1 = operation1(x1, op, x2);
+
+    cout << "Enter the second fraction:";
+    cin >> x1 >> op >> x2;
+
+    float sum2 = operation1(x1, op, x2);
+
+    x1 = sum1 + sum2;
+
+    cout << "Sum = " << x1;
+}
