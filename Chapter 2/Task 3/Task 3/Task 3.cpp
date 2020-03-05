fa384d4547a3163e73b313b920ec0dc07c6a8403
Chance20022 <chance20022@gmail.com>
2020-03-05 19:15:26 +0300

    Исправил задание

--- Chapter 2/Task 3/Task 3/Task 3.cpp
+++ Chapter 2/Task 3/Task 3/Task 3.cpp
@@ -4,13 +4,9 @@ using namespace std;
 
 int main()
 {
-	const int x1 = 10;
-	int x2 = 0;
-	x2 = 10 + 10;
-	int x3 = 20;
-
-
+	int x1 = 10;
 	cout << x1 << endl;
-	cout << x2 << endl;
-	cout << --x3 << endl;
+	x1 = x1 + 10;
+	cout << x1 << endl;
+	cout << --x1 << endl;
 }
