3625e0658466139d3a85328f1ede33ac9f93f699
Chance20022 <chance20022@gmail.com>
2020-03-06 15:06:25 +0300

    Co C++

---
+++ Chapter 2/Task 11/Task 11.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    cout << setiosflags(ios::left) << setw(17) << "Surname" << setw(17) << "name" << setw(17) << "Adress" << "City" << endl
+        << setiosflags(ios::right) << setfill('-') << setw(63) << "-" << setfill (' ')<< endl;
+    cout << setiosflags(ios::right) << "Petrov" << setw(18) << "Wasiliy" << setw(22) << "Klenovaya 16" << setw(17) << "St-Peterburg" << endl
+        << "Ivanov" << setw(17) << "Sergey" << setw(22) << "Osinovaya 3" << setw(13) << "Nahodka" << endl
+        << "Sidorov" << setw(14) << "Ivan" << setw(26) << "Berezovaya 21" << setw(15) << "Kaliningrad";
+}
