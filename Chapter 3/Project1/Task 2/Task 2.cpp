16ddfe0018aebe735080d28793abf27630a01804
Chance20022 <chance20022@gmail.com>
2020-03-12 22:26:31 +0300

    Co C++

---
+++ Chapter 3/Project1/Task 2/Task 2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char choice = ' ';
+    double number = 0;
+
+    cout << "Press 1 to convert Celsius to Fahrenheit" << endl
+        << "Press 2 to convert Fahrenheit to Celsius: ";
+
+    cin >> choice;
+    cout << endl;
+
+    switch (choice)
+    {
+    case '2':
+        cout << "Enter Fahrenheit temperature: ";
+        cin >> number;
+        cout << "Value of Celsius: " << (number - 32) * 5 / 9;
+        break;
+    case '1':
+        cout << "Enter Celsius temperature: ";
+        cin >> number;
+        cout << "Value of Celsius: " << (number * (9 / 5)) + 32;
+        break;
+    default:
+        break;
+    }
+}
