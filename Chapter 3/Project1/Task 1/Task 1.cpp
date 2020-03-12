e3f93cffa764dbd6a300317a1240dd90e459ca21
Chance20022 <chance20022@gmail.com>
2020-03-12 21:35:44 +0300

    Co C++

---
+++ Chapter 3/Project1/Task 1/Task 1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number = 0;
+
+    cout << "Enter your number ";
+
+    cin >> number;
+
+    cout << endl;
+
+    for (int i = 1, counter = 10; i < 200; i++) {
+        cout << i * number;
+        if (i == counter) {
+            cout << endl;
+            counter = counter + 10;
+        }
+    }
+}
