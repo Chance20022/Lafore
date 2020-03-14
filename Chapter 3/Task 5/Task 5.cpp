bf729b920dbf3a8385aaf5d1684bf0fd631ed671
Chance20022 <chance20022@gmail.com>
2020-03-14 22:07:44 +0300

    Исправил 5 задание

--- Chapter 3/Task 5/Task 5.cpp
+++ Chapter 3/Task 5/Task 5.cpp
@@ -6,9 +6,11 @@ using namespace std;
 int main()
 {
     string x = "X";
+    int counter = 0;
 
     for (int i = 40; i > 0; i = i - 2) {
-        cout << setfill(' ') << setw(i);
+        counter = i / 2;
+        cout << setfill(' ') << setw(counter);
         for (int z = 40; (z < i) <= 0; z--) {
             cout << x;
         }
