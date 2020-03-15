fc8219a229e5cb4ed58dc2cd45b73abd346f3fb1
Chance20022 <chance20022@gmail.com>
2020-03-15 21:51:27 +0300

    Сделал цикличным

--- Chapter 3/Task 8/Task 8.cpp
+++ Chapter 3/Task 8/Task 8.cpp
@@ -9,18 +9,31 @@ int main()
 {
 	int pounds = 0, shillings = 0, pence = 0;
 	int pound2 = 0, shillings2 = 0, pence2 = 0;
-	int counter = 2;
+	char answer = ' ';
 
-	cout << "Enter the first amount: ";
-	cin >> pounds >> shillings >> pence;
-	cout << "Enter the second amount: ";
-	cin >> pound2 >> shillings2 >> pence2;
+	while (true) {
+		cout << "Enter the first amount: ";
+		cin >> pounds >> shillings >> pence;
+		cout << "Enter the second amount: ";
+		cin >> pound2 >> shillings2 >> pence2;
 
-	pence = pence + pence2;
-	shillings = (pence / 12) + shillings + shillings2;
-	pounds = (shillings / 20) + pounds + pound2;
-	pence = pence % 12;
-	shillings = shillings % 20;
-	
-	cout << "Your amount: " << pounds << "." << shillings << "." << pence;
+		pence = pence + pence2;
+		shillings = (pence / 12) + shillings + shillings2;
+		pounds = (shillings / 20) + pounds + pound2;
+		pence = pence % 12;
+		shillings = shillings % 20;
+
+		cout << "Your amount: " << pounds << "." << shillings << "." << pence << endl;
+
+		cout << "Do you want to next? (y/n) ";
+		cin >> answer;
+		if (answer == 'n') {
+			return 0;
+		}
+		else if (answer == 'y')
+			cout << "You are next." << endl;
+		else {
+			cout << "Error!" << endl;
+		}
+	}
 }
