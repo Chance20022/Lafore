b023b292fbe4422dc388f21966fdd3ee12a31558
Chance20022 <chance20022@gmail.com>
2020-03-16 11:17:08 +0300

    11 задание

--- Chapter 3/Task_11/Task_11.cpp
+++ Chapter 3/Task_11/Task_11.cpp
@@ -6,5 +6,53 @@ using namespace std;
 
 int main()
 {
-	
+	int pounds = 0, shillings = 0, pence = 0;
+	int pound2 = 0, shillings2 = 0, pence2 = 0;
+	char answer = ' ', op = ' ';
+
+	while (true) {
+		cout << "Enter the first amount: ";
+		cin >> pounds >> shillings >> pence;
+		cout << "Enter your operation: ";
+		cin >> op;
+		cout << "Enter the second amount: ";
+		cin >> pound2 >> shillings2 >> pence2;
+
+		switch (op) {
+		case '+':
+			pence = pence + pence2;
+			shillings = (pence / 12) + shillings + shillings2;
+			pounds = (shillings / 20) + pounds + pound2;
+			pence = pence % 12;
+			shillings = shillings % 20;
+			break;
+		case '-':
+			pence = pence - pence2;
+			shillings = (pence / 12) - shillings - shillings2;
+			pounds = (shillings / 20) - pounds - pound2;
+			pence = pence % 12;
+			shillings = shillings % 20;
+			break;
+		case '*':
+			pence = pence + pence2;
+			shillings = (pence / 12) * shillings * shillings2;
+			pounds = (shillings / 20) * pounds * pound2;
+			pence = pence % 12;
+			shillings = shillings % 20;
+			break;
+		}
+
+		cout << "Your amount: " << pounds << "." << shillings << "." << pence << endl;
+
+		cout << "Do you want to next? (y/n) ";
+		cin >> answer;
+		if (answer == 'n') {
+			return 0;
+		}
+		else if (answer == 'y')
+			cout << "You are next." << endl;
+		else {
+			cout << "Error!" << endl;
+		}
+	}
 }
