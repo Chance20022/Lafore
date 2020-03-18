dd61db5c43714959c189b56e124dc7d75db6e776
Chance20022 <chance20022@gmail.com>
2020-03-18 12:38:53 +0300

    Высрал 11 задание

--- Chapter 3/Task_11/Task_11.cpp
+++ Chapter 3/Task_11/Task_11.cpp
@@ -9,14 +9,21 @@ int main()
 	int pounds = 0, shillings = 0, pence = 0;
 	int pound2 = 0, shillings2 = 0, pence2 = 0;
 	char answer = ' ', op = ' ';
+	int mult = 0;
 
 	while (true) {
 		cout << "Enter the first amount: ";
 		cin >> pounds >> shillings >> pence;
 		cout << "Enter your operation: ";
 		cin >> op;
-		cout << "Enter the second amount: ";
-		cin >> pound2 >> shillings2 >> pence2;
+		if (op == '*') {
+			cout << "Enter your number for multiolication: ";
+			cin >> mult;
+		}
+		else {
+			cout << "Enter the second amount: ";
+			cin >> pound2 >> shillings2 >> pence2;
+		}
 
 		switch (op) {
 		case '+':
@@ -27,19 +34,22 @@ int main()
 			shillings = shillings % 20;
 			break;
 		case '-':
+			pence = (pounds * 12 * 20) + (shillings * 12) + pence;
+			pence2 = (pound2 * 12 * 20) + (shillings2 * 12) + pence2;
 			pence = pence - pence2;
-			shillings = (pence / 12) - shillings - shillings2;
-			pounds = (shillings / 20) - pounds - pound2;
-			pence = pence % 12;
-			shillings = shillings % 20;
+			pounds = pence / 12 / 20;
+			shillings = pence / 12 % 20;
+			pence %= 12;
 			break;
 		case '*':
-			pence = pence + pence2;
-			shillings = (pence / 12) * shillings * shillings2;
-			pounds = (shillings / 20) * pounds * pound2;
-			pence = pence % 12;
-			shillings = shillings % 20;
+			
+			pence = (pounds * 12 * 20) + (shillings * 12) + pence;
+			pence = pence * mult;
+			pounds = pence / 12 / 20;
+			shillings = pence / 12 % 20;
+			pence %= 12;
 			break;
+			
 		}
 
 		cout << "Your amount: " << pounds << "." << shillings << "." << pence << endl;
