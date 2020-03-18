237e4503297b1d4c8515da1b48d6424ad3263c67
Chance20022 <chance20022@gmail.com>
2020-03-18 12:51:34 +0300

    Закончена галава 3

--- Chapter 3/Task 12/Task 12.cpp
+++ Chapter 3/Task 12/Task 12.cpp
@@ -8,29 +8,15 @@ int main()
     char op1, op2, op3;
 
     cin >> a >> op1 >> b;
+    cin >> op2;
+    cin >> c >> op3 >> d;
 
-    if (op1 == '/') {
-        ab = a / b;
-        cout << "Your answer - " << ab << endl;
-    }
-    else {
-        cout << "Error!";
-        return 0;
-    }
-
-    cout << "Do you want to next? (y/n) ";
-    cin >> op1;
-
-    if (op1 == 'n')
-        return 0;
-
-    cout << "Enter your expression: " << a << "/" << b << " ";
-    cin >> op2 >> c >> op3 >> d;
+    ab = a / b;
 
     switch (op2)
     {
     case '+':
-        cout << "Your answer: " << (a * d) + (b * c) << "/" << (b * d) << " or " << ab + (c/d);
+        cout << "Your answer: " << (a * d) + (b * c) << "/" << (b * d) << " or " << ab + (c / d);
         break;
     case '-':
         cout << "Your answer: " << (a * d) - (b * c) << "/" << (b * d) << " or " << ab - (c / d);
