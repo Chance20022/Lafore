99e3308f1ddd874942a31ad5a3e7e3ce75a47798
Chance20022 <chance20022@gmail.com>
2020-03-21 00:26:51 +0300

    Сделал 1 задание

---
+++ Chapter 4/Task 1/Task 1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct phone
+{
+    int CityCode = 0;
+    int stationNum = 0;
+    int numberAb = 0;
+
+};
+
+int main()
+{
+    phone phone1 = { {2290}, {9800}, {8371} };
+    phone phone2;
+
+    cout << "Enter the city code, station number and subscriber number: " << endl;
+
+    cin >> phone2.CityCode >> phone2.stationNum >> phone2.numberAb;
+
+    cout << "My number (" << phone1.CityCode << ") " << phone1.numberAb << "-" << phone1.numberAb << endl;
+    cout << "My number (" << phone2.CityCode << ") " << phone2.numberAb << "-" << phone2.numberAb << endl;
+
+}
