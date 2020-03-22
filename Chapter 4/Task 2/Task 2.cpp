21a07d4e5b79f32e6a72896020f7660498767a44
Chance20022 <chance20022@gmail.com>
2020-03-22 16:18:18 +0300

    Исправил 2 задание

--- Chapter 4/Task 2/Task 2.cpp
+++ Chapter 4/Task 2/Task 2.cpp
@@ -4,25 +4,24 @@ using namespace std;
 
 struct point {
 
-	int x1 = 0, y1 = 0;
-	int x2 = 0, y2 = 0;
-	int x3 = 0, y3 = 0;
+	int x;
+	int y;
 
 };
 
 int main()
 {
-	point x, y;
+	point p1, p2, p3;
 
 	cout << "Enter coor point p1: ";
 
-	cin >> x.x1 >> y.y1;
+	cin >> p1.x >> p1.y;
 
 	cout << "Enter coor point p2: ";
 	
-	cin >> x.x2 >> y.y2;
+	cin >> p2.x >> p2.y;
 
-	x.x3 = x.x1 + x.x2; y.y3 = y.y1 + y.y2;
+	p3.x = p1.x + p2.x; p3.y = p1.y + p2.y;
 
-	cout << "Coor points p1 + p2 = " << x.x3 << "." << y.y3;
+	cout << "Coor points p1 + p2 = " << p3.x << "." << p3.y;
 }
---
+++ Chapter 4/Task 3/Task 3.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct Distance
+{
+    int feet;
+    float inches;
+};
+
+struct Volume
+{
+    Distance length; // Длина
+    Distance width; // Ширина
+    Distance height; // Высота
+
+};
+
+int main()
+{
+    Volume volume;
+}
