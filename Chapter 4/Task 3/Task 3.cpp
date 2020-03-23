e8a393bdebc66b5e35278efc74c79718c29b3f8f
Chance20022 <chance20022@gmail.com>
2020-03-23 11:15:20 +0300

    Выполнил 3 задаие

--- Chapter 4/Task 3/Task 3.cpp
+++ Chapter 4/Task 3/Task 3.cpp
@@ -18,5 +18,11 @@ struct Volume
 
 int main()
 {
-    Volume volume;
+    Volume volume = { {10,2.12}, {5,3.32}, {2,12} };
+
+    float a1 = volume.length.feet + volume.length.inches / 12;
+    float a2 = volume.width.feet + volume.width.inches / 12;
+    float a3 = volume.height.feet + volume.height.inches / 12;
+
+    cout << a1 * a2 * a3;
 }
