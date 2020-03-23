171de05d04d8e92ef022f60588eb9e471524aba4
Chance20022 <chance20022@gmail.com>
2020-03-23 21:59:09 +0300

    4-5 задание

---
+++ Chapter 4/Task 4/Task 4.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct employee
+{
+    int numb;
+    float money;
+};
+
+int main()
+{
+    employee people1, people2, people3;
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    cout << "Insert number and money the first employee: ";
+    cin >> people1.numb >> people1.money;
+    cout << "Insert number and money the second employee: ";
+    cin >> people2.numb >> people2.money;
+    cout << "Insert number and money the third employee: ";
+    cin >> people3.numb >> people3.money << endl;
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    cout << "Employee " << people1.numb << " has benefit " << people1.money << endl;
+    cout << "Employee " << people2.numb << " has benefit " << people2.money << endl;
+    cout << "Employee " << people3.numb << " has benefit " << people3.money << endl;
+}
---
+++ Chapter 4/Task 5/Task 5.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct date {
+
+    int days, month, year;
+
+};
+
+int main()
+{
+    date date, mem;
+    char ch = '/';
+
+    cout << "Enter days/month/year: ";
+    cin >> date.days >> ch >> date.month >> ch >> date.year;
+
+    mem = { {date.days}, {date.month}, {date.year} };
+
+    cout << mem.days << ch << mem.month << ch << mem.year;
+}
