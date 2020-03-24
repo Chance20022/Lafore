166261d69405954b0ae2d8119ffc20bfc5d003d3
Chance20022 <chance20022@gmail.com>
2020-03-25 00:47:26 +0300

    Выполнил 8 задание

---
+++ Chapter 4/Task 8/Task 8.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct fraction {
+    int a, b;
+};
+
+int main()
+{
+    fraction a, b;
+    char ch = '/';
+
+    cout << "Enter the first fraction: ";
+    cin >> a.a >> ch >> a.b;
+    cout << "Enter the second fraction: ";
+    cin >> b.a >> ch >> b.b;
+
+    a.a = (a.a * b.b) + (a.b * b.a);
+    a.b = a.b * b.b;
+
+    cout << "Amaunt = " << a.a << ch << a.b;
+}
