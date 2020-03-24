a94c2c80a2787a0a126d1bde37400848943b7e9b
Chance20022 <chance20022@gmail.com>
2020-03-24 14:44:35 +0300

    6 задание

--- Chapter 4/Task 6/Task 6.cpp
+++ Chapter 4/Task 6/Task 6.cpp
@@ -1,20 +1,56 @@
-﻿// Task 6.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
 
-#include <iostream>
+using namespace std;
+
+enum etype { laborer, secretary, manager, accountant, executive, researcher };
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    etype type;
+    char ch;
+
+    cout << "Enter the first letter post (laborer, secretary, manager, accountant, executive, researcher): ";
+    cin >> ch;
+
+    switch (ch){
+    case 'l':
+        type = laborer;
+        break;
+    case 's':
+        type = secretary;
+        break;
+    case 'm':
+        type = manager;
+        break;
+    case 'a':
+        type = accountant;
+        break;
+    case 'e':
+        type = executive;
+        break;
+    case 'r':
+        type = researcher;
+        break;
+    }
+
+    switch (type) {
+    case laborer:
+        cout << "laborer";
+        break;
+    case secretary:
+        cout << "secretary";
+        break;
+    case manager:
+        cout << "manager";
+        break;
+    case accountant:
+        cout << "accountant";
+        break;
+    case executive:
+        cout << "executive";
+        break;
+    case researcher:
+        cout << "researcher";
+        break;
+    }
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
