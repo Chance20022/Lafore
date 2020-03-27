a47c98c2eb3c5acfed2559aae4c56de38d0c125b
Chance20022 <chance20022@gmail.com>
2020-03-28 02:10:42 +0300

    Закончил 4 главу

---
+++ Chapter 4/Task 12/Task 12.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct fraction {
+
+    double a, b;
+    char op1, op2;
+
+};
+int main()
+{
+    fraction a, b;
+
+    cin >> a.a >> a.op1 >> a.b;
+    cin >> a.op2;
+    cin >> b.a >> a.op1 >> b.b;
+
+    switch (a.op2)
+    {
+    case '+':
+        cout << "Your answer: " << (a.a * b.b) + (a.b * b.a) << "/" << (a.b * b.b);
+        break;
+    case '-':
+        cout << "Your answer: " << (a.a * b.b) - (a.b * b.a) << "/" << (a.b * b.b);
+        break;
+    case '*':
+        cout << "Your answer: " << (a.a * b.b) * (a.b * b.a) << "/" << (a.b * b.b);
+        break;
+    case '/':
+        cout << "Your answer: " << (a.a * b.b) / (a.b * b.a) << "/" << (a.b * b.b);
+        break;
+    default:
+        cout << "Error!";
+        break;
+    }
+}
