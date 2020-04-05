5d1c8d99cb909486922f4454a058a5df25d332ca
Chance20022 <chance20022@gmail.com>
2020-04-05 22:39:52 +0300

    Выполнил 3 задание

---
+++ Chapter 5/Task 3/Task 3.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void zeroSmaller(int&, int&);
+
+int main()
+{
+    int first, second;
+
+    cout << "Enter two number: ";
+    cin >> first >> second;
+
+    zeroSmaller(first, second);
+}
+
+void zeroSmaller(int& x, int& y) {
+
+    if (x < y) {
+        x = 0;
+        cout << x << " and " << y;
+    }
+    else if (x == y)
+        cout << "X == Y";
+    else {
+        y = 0;
+        cout << x << " and " << y;
+    }
+}
