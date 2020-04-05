0546c7a005c88dabd29ac0299d276719fdd326c6
Chance20022 <chance20022@gmail.com>
2020-04-05 20:38:48 +0300

    Исправил иерархию во в 2 главе и выполнил задание 1 из  5 главы

---
+++ Chapter 5/Task 1/Task 1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+float circarea(float);
+
+int main()
+{
+    float rad;
+
+    cout << "Enter circle radius: ";
+    cin >> rad;
+
+    rad = circarea(rad);
+
+    cout << "The area is: " << rad;
+}
+
+float circarea(float x) {
+
+    const float PI = 3.14;
+
+    return PI * x * x;
+}
