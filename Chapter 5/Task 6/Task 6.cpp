843f1b7e0af7d68d9ad0ba0a4a7fde3df6e1b482
Chance20022 <chance20022@gmail.com>
2020-04-06 13:32:58 +0300

    Выполнил 6 задание

---
+++ Chapter 5/Task 6/Task 6.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+using std::cout;
+using std::cin;
+
+struct time {
+
+    int hours, minutes, seconds;
+
+};
+
+long secs_to_time(time);
+time secs_to_time(long);
+
+int main()
+{
+    time time;
+    char ch = '/';
+
+    cout << "Enter your time (hours/minutes/senconds): ";
+    cin >> time.hours >> ch >> time.minutes >> ch >> time.seconds;
+
+    long sec = secs_to_time(time);
+    time = secs_to_time(sec);
+
+    cout << "Your time in seconds: " << sec << std::endl;
+    cout << "Your the first time: " << time.hours << ch << time.minutes << ch << time.seconds;
+
+    return 0;
+}
+
+long secs_to_time(time x) {
+    long answer = (x.hours * 3600) + (x.minutes * 60) + x.seconds;
+    return answer;
+}
+
+time secs_to_time(long x) {
+    time t1;
+    t1.hours = x / 3600;
+    t1.minutes = (x - (t1.hours * 3600)) / 60;
+    t1.seconds = x - (t1.hours * 3600) - (t1.minutes * 60);
+
+    return t1;
+}
