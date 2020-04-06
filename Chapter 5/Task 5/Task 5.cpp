98625182f678bd2bcfa18e5a9e68bae6d1d092ce
Chance20022 <chance20022@gmail.com>
2020-04-06 13:00:47 +0300

    Выполнил 5 задание

---
+++ Chapter 5/Task 5/Task 5.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using std::cout;
+using std::cin;
+
+struct time {
+    int hour;
+    int minute;
+    int second;
+};
+
+long seconds(time);
+
+int main()
+{
+    time time;
+    char ch = '/';
+
+    cout << "Enter your time (hours/minutes/senconds): ";
+    cin >> time.hour >> ch >> time.minute >> ch >> time.second;
+
+    long sec = seconds(time);
+
+    cout << "Your time in second: " << sec;
+
+}
+
+long seconds(time x) {
+    long answer = (x.hour * 3600) + (x.minute * 60) + x.second;
+    return answer;
+}
