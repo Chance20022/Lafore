b0c9948714daf3af39a102a0281d72ec77552fab
Chance20022 <57914474+Chance20022@users.noreply.github.com>
2020-05-05 11:20:09 +0300

    Выполнил 6 задание

--- Chapter 6/Task 6/Task 6.cpp
+++ Chapter 6/Task 6/Task 6.cpp
@@ -6,15 +6,15 @@ using std::cin;
 using std::endl;
 
 class date {
-private:
+public:            // Сделал публичными потому что по заданию не сказано их скрывать
     int month;
     int day;
     int year;
     char ch;
-public:
-    date() : month(0), day(0), year(0), ch('/')
-    {   /*  */
-    }
+
+    date() : month(0), day(0), year(0), ch('9')
+    {   /*  */   }
+    
 };
 
 enum etype { laborer, secretary, manager, accountant, executive, researcher };
@@ -25,15 +25,44 @@ private:
 	int numb;		// Номер сотрудника
 	float money;	// Заработок
     date d;         // Дата прнятия на работу
-    std::string type;
-    char ch;
+    char chh;
+    etype type;
 
 public:
 
-    void getemploy() {
-        cout << "Enter information about the employee (his number, salary, his position and date ofemployment): ";
+    void getemploy() { // ввод данных
 
-        cin >> numb >> ch >> money >> ch >> type >> ch >> d.month ;
+        cout << "Enter information about the employee (his number, salary, his position and date ofemployment): " << endl << " --- ";
+        
+        cin >> numb >> money >> chh >> d.month >> d.ch >> d.day >> d.ch >> d.year;
+
+    }
+
+    void transform();
+
+    void putemploy() {
+
+        switch (type) {
+        case laborer:
+            cout << "Number op employee - " << numb << ", sakary - " << money << ", position - " << "laborer, " << "date - " << d.month << d.ch << d.day << d.ch << d.year << endl;
+            break;
+        case secretary:
+            cout << "Number op employee - " << numb << ", sakary - " << money << ", position - " << "secretary, " << "date - " << d.month << d.ch << d.day << d.ch << d.year << endl;
+            break;
+        case manager:
+            cout << "Number op employee - " << numb << ", sakary - " << money << ", position - " << "manager, " << "date - " << d.month << d.ch << d.day << d.ch << d.year << endl;
+            break;
+        case accountant:
+            cout << "Number op employee - " << numb << ", sakary - " << money << ", position - " << "accountant, " << "date - " << d.month << d.ch << d.day << d.ch << d.year << endl;
+            break;
+        case executive:
+            cout << "Number op employee - " << numb << ", sakary - " << money << ", position - " << "executive, " << "date - " << d.month << d.ch << d.day << d.ch << d.year << endl;
+            break;
+        case researcher:
+            cout << "Number op employee - " << numb << ", sakary - " << money << ", position - " << "researcher, " << "date - " << d.month << d.ch << d.day << d.ch << d.year << endl;
+            break;
+        }
+
     }
 
 };
@@ -45,4 +74,39 @@ int main()
     for (int i = 0; i < 3; i++) {
         p[i].getemploy();
     }
+
+    cout << endl;
+
+    for (int i = 0; i < 3; i++) {
+        p[i].transform();
+    }
+
+    for (int i = 0; i < 3; i++) {
+        p[i].putemploy();
+    }
+}
+
+void employee::transform() {
+
+    switch (chh) {
+    case 'l':
+        type = laborer;
+        break;
+    case 's':
+        type = secretary;
+        break;
+    case 'm':
+        type = manager;
+        break;
+    case 'a':
+        type = accountant;
+        break;
+    case 'e':
+        type = executive;
+        break;
+    case 'r':
+        type = researcher;
+        break;
+    }
+
 }
