f69ca9d2a75c3223a9a729032e9435a9d1810ecd
Chance20022 <57914474+Chance20022@users.noreply.github.com>
2020-05-09 16:39:19 +0300

    1 задание

---
+++ Chapter 6/Task 1/Task 1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+class Int {
+private:
+    int i;
+public:
+    Int() : i(0)
+    {   /*  */   }
+
+    Int(int x) {
+        i = x;
+    }
+
+    Int opl(Int x) {
+        return i + x.i;
+    }
+
+    void cout_d() {
+        std::cout << "Your answer: " << i;
+    }
+};
+
+int main()
+{
+    Int x(1);
+    Int y(5);
+    Int z = x.opl(y);
+    z.cout_d();
+}
+
