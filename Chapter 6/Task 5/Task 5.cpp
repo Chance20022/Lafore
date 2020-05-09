1dd37df8d3af103538806c743cdd5e2f7e554010
Chance20022 <57914474+Chance20022@users.noreply.github.com>
2020-05-09 18:28:58 +0300

    5 задание

---
+++ Chapter 6/Task 5/Task 5.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+class date {
+private:
+	int day;
+	int month;
+	int year;
+public:
+
+	void getdate(int a, int b, int c) {
+
+		day = b;
+		month = a;
+		year = c;
+
+	}
+
+	void showdate() {
+		cout << "Your date: " << month << "/" << day << "/" << year;
+	}
+};
+
+int main()
+{
+	int a, b, c;
+	char ch;
+	date d;
+
+	cout << "Enter your date: ";
+	cin >> a >> ch >> b >> ch >> c;
+
+	d.getdate(a, b, c);
+	d.showdate();
+
+
+}
