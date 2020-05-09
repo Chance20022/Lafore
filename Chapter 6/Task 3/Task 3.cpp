f7c8995a2cf7d78783201fbcb93b8bc5f7a57c98
Chance20022 <57914474+Chance20022@users.noreply.github.com>
2020-05-09 17:14:08 +0300

    3 задание

---
+++ Chapter 6/Task 3/Task 3.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+class time {
+private:
+    int hour, min, sec;
+public:
+    time() : hour(0), min(0), sec(0)
+    {    /*   */    }
+
+    time (int x, int y, int z) : hour(x), min(z), sec(z)
+    {    /*   */    }
+
+    time add(time x) {
+        
+        time temp;
+        temp.hour = hour + x.hour;
+        temp.min = min + x.min;
+        temp.sec = sec + x.sec;
+
+        return temp;
+    }
+
+    void display() {
+        cout << "Your number: " << hour << ":" << min << ":" << sec << endl;
+    }
+};
+
+int main()
+{
+    time t1(11, 22, 45);
+    t1.display();
+    time t2(2, 21, 33);
+    t2.display();
+    time t3 = t1.add(t2);
+    t3.display();
+}
