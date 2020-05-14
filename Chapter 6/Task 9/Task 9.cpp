c22d5e352229da7b06c6cdcfa4f8bafabcdaa4b6
Chance20022 <57914474+Chance20022@users.noreply.github.com>
2020-05-15 02:05:42 +0300

    9 задание

---
+++ Chapter 6/Task 9/Task 9.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+class fraction {
+private:
+    int x, y;
+    char ch;
+public:
+
+    fraction() : x(0), y(0), ch('/')
+    {    /*   */    }
+
+    void cin_data() {
+        cin >> x >> ch >> y;
+    }
+
+    fraction add(fraction z) {
+
+        fraction temp;
+
+        temp.x = (x * z.y) + (z.x * y);
+        temp.y = y * z.y;
+
+        return temp;
+    }
+
+    void cout_data() {
+        cout << "Your answer: " << x << ch << y;
+    }
+};
+
+int main()
+{
+    fraction f1, f2;
+    fraction answer;
+
+    cout << "Enter the first fraction: ";
+    f1.cin_data();
+    cout << "enter the second fraction: ";
+    f2.cin_data();
+
+    answer = f1.add(f2);
+    answer.cout_data();
+}
