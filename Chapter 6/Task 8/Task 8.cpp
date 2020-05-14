0f5660164e258d21f532879de0a89dc972f8f32b
Chance20022 <57914474+Chance20022@users.noreply.github.com>
2020-05-15 01:37:12 +0300

    8 задание

---
+++ Chapter 6/Task 8/Task 8.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+class counter {
+private:
+    static int count;
+    int x;
+public:
+    counter() {
+        count++;
+        x = count;
+    }
+
+    void cout_data() {
+        cout << "My number is " << x << endl;
+    }
+};
+
+int counter::count = 0;
+
+int main()
+{
+    counter x1, x2, x3;
+
+    x1.cout_data();
+    x2.cout_data();
+    x3.cout_data();
+}
