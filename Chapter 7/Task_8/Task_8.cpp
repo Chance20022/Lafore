a0c2acfe1044498ed4790f042e5d6707758c2cbe
Chance20022 <57914474+Chance20022@users.noreply.github.com>
2020-08-17 22:36:15 +0300

    Задания с 7 по 9

---
+++ Chapter 7/Task_8/Task_8.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+const int LIMIT = 10;
+
+class safearay {
+private:
+	int numbers[LIMIT];
+public:
+
+	void putnull() {
+		for (int i = 0; i < LIMIT; i++) {
+			numbers[i] = 0;
+		}
+	}
+
+	void putel(int nu1, int nu2) {
+
+		if (nu1 < 0) {
+			cout << "You enter index < 0!" << endl;
+		}
+		else if (nu1 > LIMIT-1) {
+			cout << "You enter index > " << LIMIT << endl;
+		}
+		else numbers[nu1] = nu2;
+	}
+
+	int getel(int num) {
+		if (num < 0) {
+			cout << "You enter index < 0!" << endl;
+			return false;//Таким образом вызывем ошибку
+		}
+		else if (num > LIMIT-1) {
+			cout << "You enter index > " << LIMIT << "!" << endl;
+			return false; //Таким образом вызывем ошибку
+		}
+		else return numbers[num];
+	}
+
+};
+
+int main()
+{
+	safearay sa1;
+	sa1.putnull();
+	int temp = 7654;
+	sa1.putel(11, temp);
+	temp = sa1.getel(11);
+	cout << temp;
+}
---
+++ Chapter 7/Task_9/Task_9/Task_9.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+class queue {
+private:
+    enum { MAX = 10 };
+    int st[MAX];
+    int head, tail;
+public:
+    queue() : head(-1), tail(0) {}
+    void put(int var) {
+        st[tail] = var;
+        tail++;
+    }
+    int get() { 
+        head++;
+        return st[head];
+    }
+};
+
+int main()
+{
+    queue qu1;
+
+    qu1.put(11);
+    qu1.put(22);
+    cout << "1: " << qu1.get() << endl;
+    cout << "2: " << qu1.get() << endl;
+    qu1.put(33);
+    qu1.put(44);
+    qu1.put(55);
+    qu1.put(66);
+    cout << "3: " << qu1.get() << endl;
+    cout << "4: " << qu1.get() << endl;
+    cout << "5: " << qu1.get() << endl;
+    cout << "6: " << qu1.get() << endl;
+
+    return 0;
+}
