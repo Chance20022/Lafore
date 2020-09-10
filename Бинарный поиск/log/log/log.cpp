cad19b11fec7c8ead9c92b129911e20438262eb9
Chance20022 <57914474+Chance20022@users.noreply.github.com>
2020-09-11 00:19:39 +0300

    Логарифм по основанию 2

---
+++ Бинарный поиск/log/log/log.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+float log(int num) {
+
+    int counter = 0;
+    float count = num;
+
+    for (int i = num; count > 1; i--){
+        count = count / 2;
+        counter++;
+    }
+
+    if (count == 1)
+        return counter;
+    else {
+        return count;
+    }
+}
+
+int main()
+{
+    int log1 = 0;
+
+    std::cout << "Your log: ";
+    std::cin >> log1;
+    std::cout << "Answer: " << log(log1);
+
+    return 0;
+}
